fffd7c6e6335af50fbaf1ffb20f5ef49ca4e402c
Guilherme <guilhermeroasio@gmail.com>
2025-05-08 13:50:09 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const menu = documento.getElementById("menu");
+
+const cartBtn = document.getElementById("cart-btn");
+const cartModal = document.getElementById("cart-modal");
+const cartItems = document.getElementById("cart-items");
